d170ba1ac7d198b8406b0fb38a29110b24f2b00f
matthew-wright9630 <matthew.wright9630@gmail.com>
2025-07-16 16:02:46 -0400

    Update the board to include trees and 1 fire

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -3,24 +3,67 @@ import { useState } from "react";
 import "../BoardTile/BoardTile";
 import BoardTile from "../BoardTile/BoardTile";
 import "./Board.css";
+import treeImage from "../../assets/Tree.png";
+import fireImage from "../../assets/Fire.png";
 
 function Board({ gameBoard, setGameBoard, gameStarted, setGameHasStarted }) {
-  const numberOfTiles = 100;
+  let numberOfTiles = 100;
+  let numberOfTrees = 99;
+  let numberOfFire = 1;
+
+  let boardArray = new Array();
 
   function setUpBoard() {
-    console.log("button is clicked");
     setGameHasStarted(true);
-    for (let i = 0; i < numberOfTiles; i++) {
-      setUpTile(BoardTile);
+    randomizeBoard(numberOfTrees, numberOfFire);
+    setUpTile();
+  }
+
+  function setUpTile() {
+    setGameBoard(boardArray);
+  }
+
+  function randomizeBoard(numberOfTrees, numberOfFire) {
+    let selectedTileAmount = 0;
+    while (selectedTileAmount < numberOfTiles) {
+      const num = Math.floor(Math.random() * 50) + 1;
+      if (num < 50 && numberOfTrees > 0) {
+        numberOfTrees--;
+        boardArray.push({ name: "Tree", image: treeImage });
+        selectedTileAmount++;
+      } else if (num === 50 && numberOfFire > 0) {
+        numberOfFire--;
+        boardArray.push({ name: "Fire", image: fireImage });
+        selectedTileAmount++;
+      }
     }
   }
 
-  function setUpTile(tile, num) {
-    setGameBoard((tiles) => {
-      const updatedBoard = [...tiles, tile];
-      return updatedBoard;
-    });
+  function endGame() {
+    setGameHasStarted(false);
+    boardArray = [];
+    numberOfTiles = 100;
+    numberOfTrees = 100;
+    numberOfFire = 1;
   }
+
+  // function generateTileType(overrideNum) {
+  //   let num;
+  //   if (overrideNum) {
+  //     num = overrideNum;
+  //   } else {
+  //     num = Math.floor(Math.random() * 4) + 1;
+  //   }
+  //   if (num === 1) {
+  //     return "tree";
+  //   } else if (num === 2) {
+  //     return "fire";
+  //   } else if (num === 3) {
+  //     return "house";
+  //   } else {
+  //     return "river";
+  //   }
+  // }
 
   function testButton() {
     console.log(gameBoard.id);
@@ -31,9 +74,12 @@ function Board({ gameBoard, setGameBoard, gameStarted, setGameHasStarted }) {
       <button onClick={setUpBoard} className="board__start-btn">
         Start
       </button>
-      <button onClick={testButton}>Test</button>
+      {/* <button onClick={testButton}>Test</button> */}
       {gameStarted ? (
         <div>
+          <button onClick={endGame} className="board__end-btn">
+            End
+          </button>
           <div className="board__grid">
             <div className="board__first-column">
               <BoardTile title="10" gridRow="10" />
@@ -49,7 +95,7 @@ function Board({ gameBoard, setGameBoard, gameStarted, setGameHasStarted }) {
             </div>
             <div className="board__game-row">
               {gameBoard?.map((item, index) => {
-                return <BoardTile gridRow={index} key={index} />;
+                return <BoardTile gridRow={index} key={index} item={item} />;
               })}
             </div>
           </div>
--- src/components/BoardTile/BoardTile.jsx
+++ src/components/BoardTile/BoardTile.jsx
@@ -1,9 +1,17 @@
 import "./BoardTile.css";
 
-function BoardTile({ title = "", gridRow, lastRow = "false" }) {
+function BoardTile({ title = "", gridRow, lastRow = "false", item }) {
   return (
-    <div className= {`board__tile ${lastRow==="true" ? "board__empty-cell" : ""}`} >
+    <div
+      className={`board__tile ${lastRow === "true" ? "board__empty-cell" : ""}`}
+    >
       <h3 className="board-tile__name">{title}</h3>
+      {/* <p className="board-tile__type">{item?.name}</p> */}
+      {item ? (
+        <img className="board-tile__image" src={item?.image} alt={item?.name} />
+      ) : (
+        ""
+      )}
     </div>
   );
 }
