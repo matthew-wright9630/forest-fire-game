c7ddacfce94b2480235fbcae10b87b829a675355
matthew-wright9630 <matthew.wright9630@gmail.com>
2025-07-17 08:30:54 -0400

    update basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={"/matthew-wright9630.github.io/forest-fire-game/"}>
+    <BrowserRouter basename={"https://matthew-wright9630.github.io/forest-fire-game/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
