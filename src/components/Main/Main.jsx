dae9149150126a29c4171833b2ad16d05770e6c6
matthew-wright9630 <matthew.wright9630@gmail.com>
2025-07-17 09:32:40 -0400

    update the main page and include a second path

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,6 +3,7 @@ import reactLogo from "../../assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./Main.css";
 import Board from "../Board/Board";
+import { Link } from "react-router-dom";
 
 function Main({ gameBoard, setGameBoard, gameStarted, setGameHasStarted }) {
   const [count, setCount] = useState(0);
@@ -11,10 +12,7 @@ function Main({ gameBoard, setGameBoard, gameStarted, setGameHasStarted }) {
     <main>
       <section className="main">
         <h1 className="main__header">Welcome! This is the forest fire game.</h1>
-        <Link
-          className="main__link"
-          to={"/random-board"}
-        >
+        <Link className="main__link" to={"/random-board"}>
           Random Board
         </Link>
       </section>
