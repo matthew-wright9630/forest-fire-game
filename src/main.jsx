eaa25bdce0e24fac02642f5b6bc0a5ae16045f74
matthew-wright9630 <matthew.wright9630@gmail.com>
2025-09-03 11:11:59 -0400

    Updating to browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter, Router } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import App from "./components/App/App";
 import "./index.css";
@@ -8,9 +8,11 @@ import RandomBoard from "./components/RandomBoard/RandomBoard";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Routes>
-      <Route path="/" exact element={<App />} />
-      <Route path="/Random-Board" exact element={<RandomBoard />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<App />} />
+        <Route path="/Random-Board" exact element={<RandomBoard />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
