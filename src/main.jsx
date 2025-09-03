7fca6ec6de28d12568f1771b6241713a18c96a0d
matthew-wright9630 <matthew.wright9630@gmail.com>
2025-09-03 11:10:24 -0400

    Updating to browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, HashRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import App from "./components/App/App";
 import "./index.css";
 import RandomBoard from "./components/RandomBoard/RandomBoard";
