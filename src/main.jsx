6ed0bdb0ed40f302eeddfecdf8f1a061891acac7
matthew-wright9630 <matthew.wright9630@gmail.com>
2025-09-03 11:06:40 -0400

    Updating to browser router

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./components/App/App";
 import "./index.css";
+import RandomBoard from "./components/RandomBoard/RandomBoard";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </HashRouter>
+    <Routes>
+      <Route path="/" exact element={<App />} />
+      <Route path="/Random-Board" exact element={<RandomBoard />} />
+    </Routes>
   </React.StrictMode>
 );
