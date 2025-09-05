280cdf10fe81296a5b355e02ec162ad4b3e3966e
matthew-wright9630 <matthew.wright9630@gmail.com>
2025-09-05 08:02:54 -0400

    Update the homepage to make cards more visually appealing

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,25 +9,31 @@ function Main({}) {
         <h1 className="main__header">Welcome! This is the forest fire game.</h1>
         <div className="main__links">
           <Link className="main__link" to={"/forest-fire-game/random-board/"}>
-            Random Board
+            <span className="main__span">Random Board</span>
           </Link>
           <Link className="main__link" to={"/forest-fire-game/tree-board/"}>
-            Tree Board
+            <span className="main__span">Tree Board</span>
           </Link>
-          <Link className="main__link" to={"/forest-fire-game/dead-tree-board/"}>
-            Dead Tree Board
+          <Link
+            className="main__link"
+            to={"/forest-fire-game/dead-tree-board/"}
+          >
+            <span className="main__span">Dead Tree Board</span>
           </Link>
           <Link className="main__link" to={"/forest-fire-game/water-board/"}>
-            Water Board
+            <span className="main__span">Water Board</span>
           </Link>
           <Link className="main__link" to={"/forest-fire-game/house-board/"}>
-            House Board
+            <span className="main__span">House Board</span>
           </Link>
           <Link className="main__link" to={"/forest-fire-game/wind-board/"}>
-            Wind Board
+            <span className="main__span">Wind Board</span>
           </Link>
-          <Link className="main__link" to={"/forest-fire-game/fire-fighter-board/"}>
-            Fire Fighter Board
+          <Link
+            className="main__link"
+            to={"/forest-fire-game/fire-fighter-board/"}
+          >
+            <span className="main__span">Fire Fighter Board</span>
           </Link>
         </div>
       </section>
