c161a34f1adfefb3b6ad81db79f92c9cdf7927f6
matthew-wright9630 <matthew.wright9630@gmail.com>
2025-09-09 06:45:02 -0400

    Add button to BoardTile component

--- src/components/BoardTile/BoardTile.jsx
+++ src/components/BoardTile/BoardTile.jsx
@@ -1,14 +1,24 @@
 import "./BoardTile.css";
 
 function BoardTile({ title = "", gridRow, lastRow = "false", item }) {
+  function buttonClicked() {
+    console.log(item);
+  }
   return (
     <div
       className={`board__tile ${lastRow === "true" ? "board__empty-cell" : ""}`}
     >
       <h3 className="board-tile__name">{title}</h3>
       {/* <p className="board-tile__type">{item?.name}</p> */}
+
       {item ? (
-        <img className="board-tile__image" src={item?.image} alt={item?.name} />
+        <button onClick={buttonClicked} className="board-tile__button">
+          <img
+            className="board-tile__image"
+            src={item?.image}
+            alt={item?.name}
+          />
+        </button>
       ) : (
         ""
       )}
